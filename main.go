83c27caf6f4abbfcc447485811e71eb35b1f4dd1
port dev <108868128+portdeveloper@users.noreply.github.com>
2024-08-13 08:44:19 +0300

    feat(api): add Polygon chain support
    - Add Polygon chain (chainId 137) to etherscanAPIs
    - Include POLYGON_API_KEY in .env example

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func init() {
 	etherscanAPIs[10] = &GenericEtherscanAPI{BaseURL: "https://api-optimistic.etherscan.io/api", EnvKey: "OPTIMISM_API_KEY"}
 	etherscanAPIs[8453] = &GenericEtherscanAPI{BaseURL: "https://api.basescan.org/api", EnvKey: "BASE_API_KEY"}
 	etherscanAPIs[56] = &GenericEtherscanAPI{BaseURL: "https://api.bscscan.com/api", EnvKey: "BSC_API_KEY"}
+	etherscanAPIs[137] = &GenericEtherscanAPI{BaseURL: "https://api.polygonscan.com/api", EnvKey: "POLYGON_API_KEY"}
 
 	abiFetcher = NewABIFetcher(storage, etherscanAPIs)
 }
